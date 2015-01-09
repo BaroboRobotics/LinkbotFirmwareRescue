01c311af8418bde9a5ced461c19741dbc7d7f953
Harris Hancock <vortrab@gmail.com>
2015-01-08 22:14:19 -0800

    Add daemon distraction code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include "dialog.h"
 
+#include "baromesh/iocore.hpp"
+#include "baromesh/daemon.hpp"
+#include "baromesh/tcpclient.hpp"
+
+#include <boost/asio/use_future.hpp>
+
 #include <boost/scope_exit.hpp>
 
 #include <QApplication>
 #include <QDebug>
+
+#include <atomic>
+#include <chrono>
+#include <future>
+#include <thread>
 #include <cassert>
 
 static QString defaultHexFileName () {
@@ -60,8 +71,44 @@ static QString defaultHexFileName () {
 #endif
 }
 
+using boost::asio::use_future;
+
 int main(int argc, char *argv[])
 {
+    std::atomic<bool> distractTheDaemon = { true };
+    auto daemonDistracted = std::async(std::launch::async, [&] {
+        auto ioCore = baromesh::IoCore::get(false);
+        baromesh::TcpClient daemon { ioCore->ios(), boost::log::sources::logger() };
+        boost::asio::ip::tcp::resolver resolver { ioCore->ios() };
+        auto daemonQuery = decltype(resolver)::query {
+            baromesh::daemonHostName(), baromesh::daemonServiceName()
+        };
+        auto daemonIter = resolver.resolve(daemonQuery);
+        baromesh::asyncInitTcpClient(daemon, daemonIter, use_future).get();
+        rpc::asio::asyncConnect<barobo::Daemon>(daemon, std::chrono::seconds(1), use_future).get();
+        // probably don't need to worry about broadcasts
+        //daemonRunDone = rpc::asio::asyncRunClient<barobo::Daemon>(daemon, *this, use_future);
+        while (distractTheDaemon) {
+            // The idea here is to tell the daemon every m seconds to
+            // relinquish the dongle and reacquire it after n seconds. As long
+            // as m < n, the daemon will keep its hands off the serial port
+            // until distractTheDaemon becomes false.
+            asyncFire(daemon, rpc::MethodIn<barobo::Daemon>::cycleDongle{2},
+                std::chrono::seconds(1), use_future).get();
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    });
+    BOOST_SCOPE_EXIT(&distractTheDaemon, &daemonDistracted) {
+        distractTheDaemon = false;
+        try {
+            daemonDistracted.get();
+        }
+        catch (std::exception& e) {
+            qDebug() << "Exception while distracting the daemon:" << e.what();
+        }
+    } BOOST_SCOPE_EXIT_END
+
+#if 0
 #ifdef _WIN32
     auto serviceStatusToString = [] (int status) -> const char* {
         switch (status) {
@@ -120,6 +167,7 @@ int main(int argc, char *argv[])
             baromeshd = 0;
         }
     } BOOST_SCOPE_EXIT_END
+#endif
 #endif
 
     QApplication a(argc, argv);
